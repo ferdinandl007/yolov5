b99727f294abc5e30ca5e49a0c7e40941576aa2c
ferdinandl007 <ferdinandloesch@me.com>
2021-05-01 20:56:02 +0200

    fix export

--- models/export.py
+++ models/export.py
@@ -9,7 +9,9 @@ import sys
 import time
 from pathlib import Path
 
-sys.path.append(Path(__file__).parent.parent.absolute().__str__())  # to run '$ python *.py' files in subdirectories
+sys.path.append(
+    Path(__file__).parent.parent.absolute().__str__()
+)  # to run '$ python *.py' files in subdirectories
 
 import torch
 import torch.nn as nn
@@ -18,18 +20,36 @@ from torch.utils.mobile_optimizer import optimize_for_mobile
 import models
 from models.experimental import attempt_load
 from utils.activations import Hardswish, SiLU
-from utils.general import colorstr, check_img_size, check_requirements, file_size, set_logging
+from utils.general import (
+    colorstr,
+    check_img_size,
+    check_requirements,
+    file_size,
+    set_logging,
+)
 from utils.torch_utils import select_device
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default='./yolov5s.pt', help='weights path')
-    parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='image size')  # height, width
-    parser.add_argument('--batch-size', type=int, default=1, help='batch size')
-    parser.add_argument('--grid', action='store_true', help='export Detect() layer grid')
-    parser.add_argument('--device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--dynamic', action='store_true', help='dynamic ONNX axes')  # ONNX-only
-    parser.add_argument('--simplify', action='store_true', help='simplify ONNX model')  # ONNX-only
+    parser.add_argument(
+        "--weights", type=str, default="./yolov5s.pt", help="weights path"
+    )
+    parser.add_argument(
+        "--img-size", nargs="+", type=int, default=[640, 640], help="image size"
+    )  # height, width
+    parser.add_argument("--batch-size", type=int, default=1, help="batch size")
+    parser.add_argument(
+        "--grid", action="store_true", help="export Detect() layer grid"
+    )
+    parser.add_argument(
+        "--device", default="cpu", help="cuda device, i.e. 0 or 0,1,2,3 or cpu"
+    )
+    parser.add_argument(
+        "--dynamic", action="store_true", help="dynamic ONNX axes"
+    )  # ONNX-only
+    parser.add_argument(
+        "--simplify", action="store_true", help="simplify ONNX model"
+    )  # ONNX-only
     opt = parser.parse_args()
     opt.img_size *= 2 if len(opt.img_size) == 1 else 1  # expand
     print(opt)
@@ -43,10 +63,14 @@ if __name__ == '__main__':
 
     # Checks
     gs = int(max(model.stride))  # grid size (max stride)
-    opt.img_size = [check_img_size(x, gs) for x in opt.img_size]  # verify img_size are gs-multiples
+    opt.img_size = [
+        check_img_size(x, gs) for x in opt.img_size
+    ]  # verify img_size are gs-multiples
 
     # Input
-    img = torch.zeros(opt.batch_size, 3, *opt.img_size).to(device)  # image size(1,3,320,192) iDetection
+    img = torch.zeros(opt.batch_size, 3, *opt.img_size).to(
+        device
+    )  # image size(1,3,320,192) iDetection
 
     # Update model
     for k, m in model.named_modules():
@@ -58,33 +82,51 @@ if __name__ == '__main__':
                 m.act = SiLU()
         # elif isinstance(m, models.yolo.Detect):
         #     m.forward = m.forward_export  # assign forward (optional)
+    # Removes all SyncBatchNorm Layers And replaces them with BatchNorm2d,
+    # Which allows conversion of models which have been trained with the Sync flag.
+    model = revert_sync_batchnorm(model)
     model.model[-1].export = not opt.grid  # set Detect() layer grid export
     for _ in range(2):
         y = model(img)  # dry runs
-    print(f"\n{colorstr('PyTorch:')} starting from {opt.weights} ({file_size(opt.weights):.1f} MB)")
+    print(
+        f"\n{colorstr('PyTorch:')} starting from {opt.weights} ({file_size(opt.weights):.1f} MB)"
+    )
 
     # TorchScript export -----------------------------------------------------------------------------------------------
-    prefix = colorstr('TorchScript:')
+    prefix = colorstr("TorchScript:")
     try:
-        print(f'\n{prefix} starting export with torch {torch.__version__}...')
-        f = opt.weights.replace('.pt', '.torchscript.pt')  # filename
+        print(f"\n{prefix} starting export with torch {torch.__version__}...")
+        f = opt.weights.replace(".pt", ".torchscript.pt")  # filename
         ts = torch.jit.trace(model, img, strict=False)
-        ts = optimize_for_mobile(ts)  # https://pytorch.org/tutorials/recipes/script_optimized.html
+        ts = optimize_for_mobile(
+            ts
+        )  # https://pytorch.org/tutorials/recipes/script_optimized.html
         ts.save(f)
-        print(f'{prefix} export success, saved as {f} ({file_size(f):.1f} MB)')
+        print(f"{prefix} export success, saved as {f} ({file_size(f):.1f} MB)")
     except Exception as e:
-        print(f'{prefix} export failure: {e}')
+        print(f"{prefix} export failure: {e}")
 
     # ONNX export ------------------------------------------------------------------------------------------------------
-    prefix = colorstr('ONNX:')
+    prefix = colorstr("ONNX:")
     try:
         import onnx
 
-        print(f'{prefix} starting export with onnx {onnx.__version__}...')
-        f = opt.weights.replace('.pt', '.onnx')  # filename
-        torch.onnx.export(model, img, f, verbose=False, opset_version=12, input_names=['images'],
-                          dynamic_axes={'images': {0: 'batch', 2: 'height', 3: 'width'},  # size(1,3,640,640)
-                                        'output': {0: 'batch', 2: 'y', 3: 'x'}} if opt.dynamic else None)
+        print(f"{prefix} starting export with onnx {onnx.__version__}...")
+        f = opt.weights.replace(".pt", ".onnx")  # filename
+        torch.onnx.export(
+            model,
+            img,
+            f,
+            verbose=False,
+            opset_version=12,
+            input_names=["images"],
+            dynamic_axes={
+                "images": {0: "batch", 2: "height", 3: "width"},  # size(1,3,640,640)
+                "output": {0: "batch", 2: "y", 3: "x"},
+            }
+            if opt.dynamic
+            else None,
+        )
 
         # Checks
         model_onnx = onnx.load(f)  # load onnx model
@@ -94,34 +136,47 @@ if __name__ == '__main__':
         # Simplify
         if opt.simplify:
             try:
-                check_requirements(['onnx-simplifier'])
+                check_requirements(["onnx-simplifier"])
                 import onnxsim
 
-                print(f'{prefix} simplifying with onnx-simplifier {onnxsim.__version__}...')
-                model_onnx, check = onnxsim.simplify(model_onnx,
-                                                     dynamic_input_shape=opt.dynamic,
-                                                     input_shapes={'images': list(img.shape)} if opt.dynamic else None)
-                assert check, 'assert check failed'
+                print(
+                    f"{prefix} simplifying with onnx-simplifier {onnxsim.__version__}..."
+                )
+                model_onnx, check = onnxsim.simplify(
+                    model_onnx,
+                    dynamic_input_shape=opt.dynamic,
+                    input_shapes={"images": list(img.shape)} if opt.dynamic else None,
+                )
+                assert check, "assert check failed"
                 onnx.save(model_onnx, f)
             except Exception as e:
-                print(f'{prefix} simplifier failure: {e}')
-        print(f'{prefix} export success, saved as {f} ({file_size(f):.1f} MB)')
+                print(f"{prefix} simplifier failure: {e}")
+        print(f"{prefix} export success, saved as {f} ({file_size(f):.1f} MB)")
     except Exception as e:
-        print(f'{prefix} export failure: {e}')
+        print(f"{prefix} export failure: {e}")
 
     # CoreML export ----------------------------------------------------------------------------------------------------
-    prefix = colorstr('CoreML:')
+    prefix = colorstr("CoreML:")
     try:
         import coremltools as ct
 
-        print(f'{prefix} starting export with coremltools {ct.__version__}...')
+        print(f"{prefix} starting export with coremltools {ct.__version__}...")
         # convert model from torchscript and apply pixel scaling as per detect.py
-        model = ct.convert(ts, inputs=[ct.ImageType(name='image', shape=img.shape, scale=1 / 255.0, bias=[0, 0, 0])])
-        f = opt.weights.replace('.pt', '.mlmodel')  # filename
+        model = ct.convert(
+            ts,
+            inputs=[
+                ct.ImageType(
+                    name="image", shape=img.shape, scale=1 / 255.0, bias=[0, 0, 0]
+                )
+            ],
+        )
+        f = opt.weights.replace(".pt", ".mlmodel")  # filename
         model.save(f)
-        print(f'{prefix} export success, saved as {f} ({file_size(f):.1f} MB)')
+        print(f"{prefix} export success, saved as {f} ({file_size(f):.1f} MB)")
     except Exception as e:
-        print(f'{prefix} export failure: {e}')
+        print(f"{prefix} export failure: {e}")
 
     # Finish
-    print(f'\nExport complete ({time.time() - t:.2f}s). Visualize with https://github.com/lutzroeder/netron.')
+    print(
+        f"\nExport complete ({time.time() - t:.2f}s). Visualize with https://github.com/lutzroeder/netron."
+    )
